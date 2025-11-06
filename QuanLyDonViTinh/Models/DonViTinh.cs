aad19932ba71105db6a822b969adbba62a4f264d
lenhuttruong2004 <letruongbt2018@gmail.com>
2025-11-06 10:15:57 +0700

    tự động báo lỗi hoặc chặn không cho nhập tiếp khi đạt 50 ký tự.

--- QuanLyDonViTinh/Models/DonViTinh.cs
+++ QuanLyDonViTinh/Models/DonViTinh.cs
@@ -1,20 +1,13 @@
-﻿/* Dòng này rất quan trọng để dùng [Required] */
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
-/* 'QuanLyDonViTinh' là tên dự án của bạn */
 namespace QuanLyDonViTinh.Models
 {
     public class DonViTinh
     {
-        /* Các thuộc tính này phải đặt tên 
-           GIỐNG HỆT TÊN CỘT trong SQL Server 
-        */
         public int Ma_Don_Vi_Tinh { get; set; }
 
-        /* [Required] để xử lý ràng buộc "không được rỗng"
-           ErrorMessage là thông báo sẽ hiện ra nếu người dùng không nhập
-        */
         [Required(ErrorMessage = "Tên đơn vị tính không được để trống")]
+        [StringLength(50, ErrorMessage = "Tên đơn vị tính không được quá 50 ký tự")]
         public string Ten_Don_Vi_Tinh { get; set; }
 
         public string Ghi_Chu { get; set; }
